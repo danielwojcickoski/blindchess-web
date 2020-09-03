467a2e02a918f504c97c4f3fcc24fa70d74b0d3c
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2020-09-02 21:02:53 -0300

    Create members page

--- src/pages/MembersPage/index.jsx
+++ src/pages/MembersPage/index.jsx
@@ -4,11 +4,34 @@ import Navbar from '../../components/Navbar';
 
 import './styles.css';
 
-export default function Homepage() {
+import profileDani from '../../assets/images/profile-dani.jpeg';
+import profileGusta from '../../assets/images/profile-gusta.jpeg';
+import profileRafa from '../../assets/images/profile-rafa.jpeg';
+
+export default function MembersPage() {
   return (
-    <div className="container members-page-content">
+    <div className="members-page-content container">
       <Navbar />
 
+      <section className="content-body">
+        <div className="member">
+          <img src={profileDani} alt="" className="member-img" />
+          <p className="member-name">Daniel Wojcickoski</p>
+          <p className="member-description">Técnico em Mecatrônica<br/>IFSul - Campus Charqueadas</p>
+        </div>
+
+        <div className="member">
+          <img src={profileGusta} alt="" className="member-img" />
+          <p className="member-name">Gustavo Diersmann</p>
+          <p className="member-description">Técnico em Mecatrônica<br/>IFSul - Campus Charqueadas</p>
+        </div>
+
+        <div className="member">
+          <img src={profileRafa} alt="" className="member-img" />
+          <p className="member-name">Rafael Vargas</p>
+          <p className="member-description">Eng. Controle e Automação<br/>UFRGS</p>
+        </div>
+      </section>
     </div >
   );
 }
